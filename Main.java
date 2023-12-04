cb68ad409ebcffc5a5992ae87a1a2812d77af827
kauanzeraaa <cc22316@g.unicamp.br>
2023-12-04 16:07:59 -0300

    last changes

--- Main.java
+++ Main.java
@@ -6,15 +6,21 @@ public class Main {
     static String s;
 
     public static void main(String[] args) throws IOException {
+        long tempoInicial = System.currentTimeMillis();
         BufferedReader br = new BufferedReader(new FileReader("teste.txt"));
         String line;
+        System.out.println("");
         while ((line = br.readLine()) != null) {
             s = line;
             pos = 0;
             Pair result = solve();
+            
             System.out.println(result.first + " " + result.second);
         }
         br.close();
+
+        long tempoFinal = System.currentTimeMillis();
+        System.out.printf("\nTempo de execução: " + "%.3f ms%n", (tempoFinal - tempoInicial) / 1000d);
     }
 
     static class Pair {
